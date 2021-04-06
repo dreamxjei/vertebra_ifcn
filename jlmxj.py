f8b357a40b81b4b2ccfa8b8347494ea993d4ac6f
dreamxjei <mrJinch@gmail.com>
2021-04-06 14:25:52 -0400

    removed scratch

--- jlmxj.py
+++
@@ -1,4 +0,0 @@
-from IPython.display import display, HTML
-with open('/home-2/jwei9@jhu.edu/mxj_jupyter.css') as f:
-    css = f.read().replace(';', ' !important;')
-display(HTML('<style type="text/css">%s</style>Customized changes loaded.'%css))
